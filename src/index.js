d1bfb1dbec9eb0d1154ea616edd2177690570ba2
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-07-23 16:06:47 -0600

    imports & exports setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("HELLO FROM WEBPACK!!!")
+import { run } from './app';
+import { AlertService } from './alert.service';
+import { ComponentService } from './component.service';
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
